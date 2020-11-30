c08124efd23caf920523382343507187d74c4f6e
michaelvong <mvong006@ucr.edu>
2020-11-30 12:49:25 -0800

    Created pure virtual function for create_iterator

--- base.hpp
+++ base.hpp
@@ -2,6 +2,7 @@
 #define __BASE_HPP__
 
 #include <string>
+#include "iterator.hpp"
 
 class Base {
     public:
@@ -11,6 +12,7 @@ class Base {
         /* Pure Virtual Functions */
         virtual double evaluate() = 0;
         virtual std::string stringify() = 0;
+        virtual Iterator* create_iterator() = 0;
 };
 
 #endif //__BASE_HPP__
