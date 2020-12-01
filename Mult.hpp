4712d574d1e0c3ca51acad381457aa7d4bcc3500
michaelvong <mvong006@ucr.edu>
2020-11-30 23:02:32 -0800

    Modified file to work with iterator

--- Mult.hpp
+++ Mult.hpp
@@ -7,6 +7,8 @@ class Mult : public Base {
     double value1;
     double value2;
     double testfinal;
+    Base* leftnode;
+    Base* rightnode;
    
         Mult(Base* test1,Base* test2) : Base() { 
         value1  = test1->evaluate();
@@ -16,6 +18,19 @@ class Mult : public Base {
        virtual double evaluate()  {return testfinal;} 
         virtual std::string stringify()
         {return std::to_string(value1) + "*" + std::to_string(value2);}
+
+        Iterator* create_iterator(){
+                     Iterator* it = new BinaryIterator(this);
+                return it;
+        }
+
+        Base* get_left(){
+            return this->leftnode;
+        }
+
+        Base* get_right(){
+            return this->rightnode;
+        }
 };
 
 #endif //__MULT_HPP__
