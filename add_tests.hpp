248d71f5db912b3609eb6012c34318b97b228a1b
michaelvong <mvong006@ucr.edu>
2020-12-01 02:31:26 -0800

    Added more tests

--- add_tests.hpp
+++ add_tests.hpp
@@ -8,6 +8,9 @@
 #include "sub.hpp"
 #include "Mult.hpp"
 #include "visitor.hpp"
+#include "Rand.hpp"
+#include "div.hpp"
+#include "pow.hpp"
 
 TEST(AddIteratorTest, BasicAddTest){
     Base* three = new Op(3);
@@ -150,5 +153,71 @@ TEST(VisitorTest, AnotherVisitTest){
     EXPECT_EQ(temp->is_done(), true);
     EXPECT_EQ(visitor->add_count(), 3);
     EXPECT_EQ(visitor->op_count(), 4);
+}
+
+TEST(RandTest, BasicRandTest){
+    Rand* rand = new Rand(3);
+    Iterator* temp = rand->create_iterator();
+    EXPECT_EQ(temp->is_done(), true);
+    EXPECT_EQ(temp->current(), nullptr);
+}
+
+TEST(DivTest, BasicDivTest){
+    Base* six = new Op(6);
+    Base* three = new Op(3);
+    Div* div = new Div(six, three);
+    Iterator* temp = div->create_iterator();
+    temp->first();
+    EXPECT_EQ(temp->current()->evaluate(), 6);
+}
+
+TEST(PowTest, BasicDivTest){
+    Base* two = new Op(2);
+    Base* three = new Op(3);
+    Pow* pow = new Pow(three, two);
+    Iterator* temp = pow->create_iterator();
+    temp->first();
+    EXPECT_EQ(temp->current()->evaluate(), 3);
+    temp->next();
+    EXPECT_EQ(temp->current()->evaluate(), 2);
+}
+
+TEST(PreorderTest, MainTest){
+    Base* three = new Op(3);
+    Base* two = new Op(2);
+    Base* four = new Op(4);
+    Base* six = new Op(6);
+
+    Add* add = new Add(three, two);
+    Add* add2 = new Add (four, six);
+    Add* add3 = new Add(add, add2);
+    Sub* sub = new Sub(add3, four);
+    Div* div = new Div(sub, two);
+    Add* add4 = new Add(div, six);
+    Mult* mult = new Mult(add4, two);
+    Pow* pow = new Pow(mult, two);
+    Rand* rand = new Rand(2);
+    Sub* sub2 = new Sub(pow, rand);
+
+    CountVisitor* visitor = new CountVisitor();
+    Iterator* temp = new PreorderIterator(sub2);
+    sub2->accept(visitor);
+
+    temp->first();
+    while(!(temp->is_done())){
+        if(temp->current() != nullptr){
+            temp->current()->accept(visitor);
+            EXPECT_EQ(temp->is_done(), false);
+        }
+        temp->next();
+    }
+    EXPECT_EQ(temp->is_done(), true);
+    EXPECT_EQ(visitor->add_count(), 4);
+    EXPECT_EQ(visitor->div_count(), 1);
+    EXPECT_EQ(visitor->sub_count(), 2);
+    EXPECT_EQ(visitor->mult_count(), 1);
+    EXPECT_EQ(visitor->rand_count(), 1);
+    EXPECT_EQ(visitor->pow_count(), 1);
+    EXPECT_EQ(visitor->op_count(), 9);
 }
 #endif
