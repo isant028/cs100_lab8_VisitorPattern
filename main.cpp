314a518eee034d0045122af2056c5b5b198625b8
michaelvong <mvong006@ucr.edu>
2020-12-01 00:11:09 -0800

    No changes

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
+#ifndef MAIN_CPP
+#define MAIN_CPP
 #include "gtest/gtest.h"
 
-#include "add.hpp"
 #include "add_tests.hpp"
 int main(int argc, char**argv){
     ::testing::InitGoogleTest(&argc,argv);
     return RUN_ALL_TESTS();
 }
+
+#endif
