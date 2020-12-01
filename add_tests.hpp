e0dddf6ff608abbfc6b0e3ad64e834fafad41811
michaelvong <mvong006@ucr.edu>
2020-12-01 00:11:27 -0800

    Added more tests

--- add_tests.hpp
+++ add_tests.hpp
@@ -5,6 +5,8 @@
 #include "base.hpp"
 #include "iterator.hpp"
 #include "add.hpp"
+#include "sub.hpp"
+#include "Mult.hpp"
 TEST(AddIteratorTest, BasicAddTest){
     Base* three = new Op(3);
     Base* seven = new Op(7);
@@ -48,6 +50,61 @@ TEST(OpIteratorTest, BasicOperandTest){
     Iterator* temp = three->create_iterator();
     EXPECT_EQ(temp->is_done(), true);
     EXPECT_EQ(temp->current(), nullptr);
+}
+
+
+TEST(SubIteratorTest, BasicSubTest){
+    Base* three = new Op(3);
+    Base* seven = new Op(7);
+    Sub* tester = new Sub(seven, three);
+    Iterator* temp = tester->create_iterator();
+    temp->first();
+    EXPECT_EQ(temp->current()->evaluate(), 7);
+    temp->next();
+    EXPECT_EQ(temp->current()->evaluate(), 3);
+    temp->next();
+    EXPECT_EQ(temp->is_done(), true);
+    EXPECT_EQ(temp->current(), nullptr);
+
+}
+
+
+TEST(MultIteratorTest, BasicMultTest){
+    Base* three = new Op(3);
+    Base* seven = new Op(7);
+    Mult* test = new Mult(three, seven);
+    Iterator* temp = test->create_iterator();
+    EXPECT_EQ(temp->is_done(), false);
+    temp->first();
+    EXPECT_EQ(temp->current()->evaluate(), 3);
+    EXPECT_EQ(temp->is_done(), false);
+    temp->next();
+    EXPECT_EQ(temp->is_done(), false);
+    EXPECT_EQ(temp->current()->evaluate(), 7);
+    temp->next();
+    EXPECT_EQ(temp->is_done(), true);
+    EXPECT_EQ(temp->current(), nullptr);
+
+}
+
+TEST(DivIteratorTest, BasicDivTest){
+    Base* four = new Op(4);
+    Base* eight = new Op(8);
+    Mult* test = new Mult(four, eight);
+    Iterator* temp = test->create_iterator();
+    temp->first();
+    EXPECT_EQ(temp->is_done(), false);
+    EXPECT_EQ(temp->current()->evaluate(), 4);
+    temp->next();
+    EXPECT_EQ(temp->is_done(), false);
+    EXPECT_EQ(temp->current()->evaluate(), 8);
+    temp->next();
+    EXPECT_EQ(temp->is_done(), true);
+    EXPECT_EQ(temp->current(), nullptr);
+
+
+
+
 }
 
 #endif
