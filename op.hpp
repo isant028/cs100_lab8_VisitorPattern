f5ce325429baee141a8143613436e9b6732cb292
michaelvong <mvong006@ucr.edu>
2020-11-30 22:56:09 -0800

    Created and defined virtual functions to work with iterator

--- op.hpp
+++ op.hpp
@@ -2,7 +2,7 @@
 #define __OP_HPP__
 
 #include "base.hpp"
-
+#include "iterator.hpp"
 #include <string>
 
 class Op : public Base {
@@ -12,7 +12,19 @@ class Op : public Base {
         virtual std::string stringify() {
 		std::string value2 = std::to_string(val);
 	       	return value2; 
-	}
+        }
+        virtual Iterator* create_iterator(){
+            Iterator* it = new NullIterator(this);
+            return it;
+        }
+
+        virtual Base* get_left(){
+            return nullptr;
+        }
+
+        virtual Base* get_right(){
+            return nullptr;
+        }
    private:
 	double val;
 };
