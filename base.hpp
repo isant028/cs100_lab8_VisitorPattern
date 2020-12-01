44417e841cce5e1afb381f8a2c2d227958eb2aae
michaelvong <mvong006@ucr.edu>
2020-11-30 21:41:11 -0800

    Added pure virtual functions

--- base.hpp
+++ base.hpp
@@ -14,7 +14,7 @@ class Base {
         virtual std::string stringify() = 0;
         virtual Iterator* create_iterator() = 0;
         virtual Base* get_left() = 0;
-        virtual Base* get_right() = 0;
+        virtual Base* get_right() = 0;  
 };
 
 #endif //__BASE_HPP__
